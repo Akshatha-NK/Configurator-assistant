c43bb8ba9e2b7568ef49512c9cd149b7ee69c2a3
Akshatha-NK <61462808+Akshatha-NK@users.noreply.github.com>
2026-07-16 09:50:42 +0530

    Update applcatn.py

--- applcatn.py
+++ applcatn.py
@@ -13,31 +13,31 @@ configurator_context = """
 #Prompt users if they want to see examples if they say yes then only provide."""
 """
 """
-"""
-st.title("Oracle Configurator Assistant")
 
-if "messages" not in st.session_state:
-    st.session_state.messages=[]
+#st.title("Oracle Configurator Assistant")
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+#if "messages" not in st.session_state:
+   # st.session_state.messages=[]
 
-if question:= st.chat_input("Ask a configurator question...."):
-    st.session_state.messages.append({"role":"user","content": question})
-    st.chat_message("user").write(question)
+#for msg in st.session_state.messages:
+   # st.chat_message(msg["role"]).write(msg["content"])
 
-    response = Client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents=question,
-        config = types.GenerateContentConfig(system_instruction=configurator_context,
-                                             temperature=0.3,
-                                             max_output_tokens=5000)
-    )
+#if question:= st.chat_input("Ask a configurator question...."):
+    #st.session_state.messages.append({"role":"user","content": question})
+   # st.chat_message("user").write(question)
 
-    answer = response.text
-    st.session_state.messages.append({"role":"assistant","content":answer})
-    st.chat_message("assistant").write(answer)
-    """
+   # response = Client.models.generate_content(
+        #model = "gemini-2.5-flash",
+       # contents=question,
+        #config = types.GenerateContentConfig(system_instruction=configurator_context,
+                                           #  temperature=0.3,
+                                        #     max_output_tokens=5000)
+ #   )
+
+    #answer = response.text
+   # st.session_state.messages.append({"role":"assistant","content":answer})
+    #st.chat_message("assistant").write(answer)
+    
 
 import streamlit as st
 from google import genai
